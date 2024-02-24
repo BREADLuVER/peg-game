cd35f04031b163d59a9df6112cde56c397c74255
BREADLuVER <ry2050@nyu.edu>
2024-02-24 12:21:24 -0500

    backend

--- back_end.py
+++ back_end.py
@@ -1,41 +1,34 @@
-def read_dp_output(filename):
-    assignments = []
+def read_dp_solution_and_legend(filename):
+    solution = {}
+    legend = {}
+    start_reading_solution = False
     with open(filename, 'r') as file:
         for line in file:
-            parts = line.strip().split()
-            if parts[0] == '0':
-                break
-            var, val = int(parts[0]), parts[1]
-            assignments.append((var, val))
-    return assignments
+            line = line.strip()
+            if not line or line == '0':
+                start_reading_solution = True
+                continue
+            if start_reading_solution:
+                if ' ' in line:
+                    var, val = line.split()
+                    solution[int(var)] = val == 'T'
+                else:
+                    var, description = line.split(maxsplit=1)
+                    legend[int(var)] = description
+    return solution, legend
 
-def decode_action(var):
-    action_mapping = {
-        6: "Jump(1,2,3,1)",
-    }
-    return action_mapping.get(var, "Unknown action")
-
-def apply_move(game_state, move):
-    return game_state
-
-def translate_to_moves(assignments):
-    moves = []
-    game_state = {}
-    for var, val in assignments:
-        if val == 'T':
-            action = decode_action(var)
-            if action != "Unknown action":
-                game_state = apply_move(game_state, action)
-                moves.append(action)
+def generate_moves_from_dp_output(filename):
+    solution, legend = read_dp_solution_and_legend(filename)
+    moves = [legend[var] for var, is_true in solution.items() if is_true and var in legend]
     return moves
 
-def main(dp_output_filename):
-    assignments = read_dp_output(dp_output_filename)
-    moves = translate_to_moves(assignments)
-    print("Sequence of Moves to Solve the Peg Game:")
+def main():
+    filename = 'combined_output.txt'
+    moves = generate_moves_from_dp_output(filename)
     for move in moves:
         print(move)
 
-dp_output_filename = 'dp_output.txt'
-main(dp_output_filename)
+if __name__ == "__main__":
+    main()
+
 
