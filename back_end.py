114bf35f576f4c9ac2e9b9bca03d9c020775aff0
BREADLuVER <ry2050@nyu.edu>
2024-02-24 13:04:37 -0500

    backend mod

--- back_end.py
+++ back_end.py
@@ -1,36 +1,39 @@
 def read_dp_output(filename):
     solution = {}
     legend = {}
-    reading_solution = True  # Start by reading the solution part
+    reading_solution = True
     with open(filename, 'r') as file:
         for line in file:
-            if line.strip() == '0':  # Switch to reading the legend
+            line = line.strip()
+            if line == '0':
                 reading_solution = False
                 continue
             if reading_solution:
                 var, val = line.split()
                 solution[int(var)] = val == 'T'
             else:
-                var, description = line.split(maxsplit=1)
+                var, description = line.strip().split(maxsplit=1)
                 legend[int(var)] = description
     return solution, legend
 
-def interpret_solution(solution, legend):
-    moves = []
+def filter_feasible_jumps(solution, legend):
+    jumps = []
     for var, taken in solution.items():
         if taken and var in legend and legend[var].startswith("Jump"):
-            moves.append(legend[var])
-    return moves
+            jump_details = legend[var]
+            # Additional logic to filter out infeasible jumps could be implemented here
+            jumps.append(jump_details)
+    return jumps
 
 def generate_moves(dp_output_filename):
     solution, legend = read_dp_output(dp_output_filename)
-    moves = interpret_solution(solution, legend)
-    return moves
+    feasible_jumps = filter_feasible_jumps(solution, legend)
+    return feasible_jumps
 
 def main():
     dp_output_filename = 'dp_output.txt'
     moves = generate_moves(dp_output_filename)
-    print("Sequence of jumps to solve the peg game:")
+    print("Sequence of feasible jumps to solve the peg game:")
     for move in moves:
         print(move)
 
