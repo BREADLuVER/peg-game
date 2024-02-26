682c8209625aeda83232ad632359744208e13042
BREADLuVER <ry2050@nyu.edu>
2024-02-26 02:19:02 -0500

    dp

--- dp_algorithm.py
+++ dp_algorithm.py
@@ -1,129 +1,54 @@
-def read_clauses_and_legend_from_file(filename):
-    clauses = []
-    back_matter = ''
-    reading_clauses = True
-    with open(filename, 'r') as file:
-        for line in file:
-            line = line.strip()
-            if line == '0':
-                reading_clauses = False
-                continue
-            if reading_clauses:
-                clause = list(map(int, line.split()))
-                clauses.append(clause)
-            else:
-                back_matter += line + '\n'
-    return clauses, back_matter
+def parse_input(input_filename):
+    with open(input_filename, 'r') as file:
+        lines = file.readlines()
+        clauses = []
+        for line in lines:
+            if line.strip() == '0':
+                break
+            clause = list(map(int, line.split()))
+            clauses.append(clause)
+        max_atom = max(abs(lit) for clause in clauses for lit in clause)
+        return clauses, max_atom
 
-def apply_unit_clause(clauses, unit):
-    new_clauses = []
-    for clause in clauses:
-        if unit in clause:
-            continue
-        if -unit in clause:
-            new_clause = [x for x in clause if x != -unit]
-            new_clauses.append(new_clause)
-        else:
-            new_clauses.append(clause)
-    return new_clauses
-
-def apply_pure_literal_rule(clauses):
-    new_clauses = []
-    pure_literals = set()
-    for clause in clauses:
-        for literal in clause:
-            if -literal not in pure_literals:
-                pure_literals.add(literal)
-    for clause in clauses:
-        if not any(literal in pure_literals for literal in clause):
-            new_clauses.append(clause)
-    return new_clauses
-
-def choose_literal(clauses):
-    for clause in clauses:
-        if clause:  # Ensure the clause is not empty
-            for literal in clause:
-                return literal
-    return None
-
-def assign(clauses, literal):
-    new_clauses = []
-    for clause in clauses:
-        if literal in clause:
-            continue
-        if -literal in clause:
-            new_clause = [x for x in clause if x != -literal]
-            if not new_clause:  # Unsatisfiable if empty clause is produced
-                return None
-            new_clauses.append(new_clause)
-        else:
-            new_clauses.append(clause)
-    return new_clauses
-
-
-def dp_solve(clauses, assignments={}):
-    # Base case checks
-    if any(len(clause) == 0 for clause in clauses):
-        return None  # Indicates an unsatisfiable clause set
-
+def davis_putnam(clauses, max_atom, assignments={}, atom=1):
     if not clauses:
-        return assignments  # All clauses satisfied
+        for a in range(atom, max_atom + 1):
+            if a not in assignments:
+                assignments[a] = True
+        return assignments
+    if any(not clause for clause in clauses):
+        return None
 
-    # Unit Propagation
-    while True:
-        unit_clauses = [clause for clause in clauses if len(clause) == 1]
-        if not unit_clauses:
-            break
-        for unit_clause in unit_clauses:
-            unit = unit_clause[0]
-            assignments[abs(unit)] = unit > 0
-            clauses = apply_unit_clause(clauses, unit)
-            if any(len(clause) == 0 for clause in clauses):
-                return None
+    _, literal = min((len([lit for lit in clause if abs(lit) == abs(clause[0])]), clause[0]) for clause in clauses)
+    print(f'Selected literal: {literal}')  # Debug print
 
-    # Choose a variable (literal) to assign
-    literal = choose_literal(clauses)
-    if literal is None:
-        return assignments  # All literals assigned
-
-    # Try assigning the literal True
-    new_assignments = assignments.copy()
-    new_assignments[abs(literal)] = literal > 0
-    result = dp_solve(apply_unit_clause(clauses, literal), new_assignments)
+    new_clauses = simplify(clauses, literal)
+    result = davis_putnam(new_clauses, max_atom, {**assignments, abs(literal): literal > 0}, atom + 1)
     if result is not None:
         return result
 
-    # Try assigning the literal False
-    new_assignments = assignments.copy()
-    new_assignments[abs(literal)] = not (literal > 0)
-    return dp_solve(apply_unit_clause(clauses, -literal), new_assignments)
-
-
-
-def find_unit_clauses(clauses):
-    """Finds all unit clauses in the current clause list."""
-    return [clause[0] for clause in clauses if len(clause) == 1]
-
-def write_solution_and_back_matter_to_file(solution, back_matter, filename):
-    with open(filename, 'w') as file:
-        if solution is None:
-            file.write("0\n") 
-            print('no') # Adjusted to match the specified output format for unsatisfiability
-        else:
-            for var in sorted(solution):
-                file.write(f"{var} {'T' if solution[var] else 'F'}\n")
-            file.write('0\n')  # Ensure this line is present before the back matter
-        file.write(back_matter)
-
-
+    new_clauses = simplify(clauses, -literal)
+    return davis_putnam(new_clauses, max_atom, {**assignments, abs(literal): literal < 0}, atom + 1)
 
 def solve_sat_from_file(input_filename, output_filename):
-    clauses, back_matter = read_clauses_and_legend_from_file(input_filename)
-    solution = dp_solve(clauses)
-    write_solution_and_back_matter_to_file(solution, back_matter, output_filename)
+    clauses, max_atom = parse_input(input_filename)
+    solution = davis_putnam(clauses, max_atom)
+    print(f'Solution: {solution}')  # Debug print
+    write_output(output_filename, solution)
 
-# Example usage
+def simplify(clauses, literal):
+    clauses = [clause for clause in clauses if literal not in clause]
+    return [[lit for lit in clause if lit != -literal] for clause in clauses]
+
+def write_output(output_filename, solution):
+    with open(output_filename, 'w') as file:
+        if solution is None:
+            file.write('0\n')
+        else:
+            for atom, value in solution.items():
+                file.write(f'{atom} {"T" if value else "F"}\n')
+            file.write('0\n')
+
 input_filename = 'front_end_output.txt'
 output_filename = 'dp_output.txt'
 solve_sat_from_file(input_filename, output_filename)
-
